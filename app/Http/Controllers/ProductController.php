9f8b3f8951dcff5a679237e58cd39a9c2b35d8ee
thuantruong <thuan20132000@gmail.com>
2020-03-27 14:14:49 +0700

    Product Destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -110,5 +110,8 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         //
+        $product->delete();
+        return response(null,HttpResponse::HTTP_NO_CONTENT);
+
     }
 }
