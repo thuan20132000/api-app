75cbd2030535c930e3edbeb5c5c257ae8991a677
TruongThuan <thuan20202000@gmail.com>
2020-03-31 13:07:09 +0700

    Add imageUrl and category_id in products Migrations

--- database/migrations/2020_03_26_175455_create_products_table.php
+++ database/migrations/2020_03_26_175455_create_products_table.php
@@ -16,10 +16,12 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('imageUrl');
             $table->text('detail');
             $table->integer('price');
             $table->integer('stock');
             $table->integer('discount');
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
         });
     }
